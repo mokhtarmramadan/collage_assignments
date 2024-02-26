40e461088fab74bf1ae0e3319b4e43469b77c1f6
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-26 08:09:34 +0200

    Added even or odd

---
+++ Ai/0x04-even_odd.py
@@ -0,0 +1,9 @@
+# Even or Odd
+
+number = float(input("Number: "))
+if number > 0:
+    print("{} is even".format(number))
+elif number < 0:
+    print("{} is odd".format(number))
+else:
+    print("{} is zero".format(number))
