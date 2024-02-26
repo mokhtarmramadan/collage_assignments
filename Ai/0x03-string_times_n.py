2f00eca71042b18220be18bca6fc7ce0d9576fae
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-26 08:05:32 +0200

    Added string_times_n'

---
+++ Ai/0x03-string_times_n.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate the sum of three given
+# numbers, if the values are equal then return thrice of their sum
+n = int(input("n: "))
+string = str(input("String: "))
+if n > 0:
+    print(string * n)
