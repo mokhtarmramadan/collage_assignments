b501bea980c07791fac7b386caa7ca85210b76c2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-26 08:17:54 +0200

    Added frequency

---
+++ Ai/0x08-char_in_str.py
@@ -0,0 +1,6 @@
+# Write a Python program to count the number of characters
+#(character frequency) in a string.
+string = input("String: ")
+char = input("Char: ")
+frequency = string.count(char)
+print(frequency)
